4c9e0598a4bc7f16c51de0cb83f1a614dac6eb05
Tanick-tech <ndtantsn@gmail.com>
2026-05-07 09:53:26 +0700

    Part 13: Negamax + AlphaBetaPruning
    In this part, we have coded a new algorithm for AI to think: Negamax. And to increase the speed, we have added AlphaBetaPruning

--- SmartMoveFinder.py
+++ SmartMoveFinder.py
@@ -1,12 +1,12 @@
 import random
 import settings
 
-
 '''
 The goal for each sides:
 For white pieces: get the score as high as possible (positive numbers)
 For black pieces: get the score as low as possible (negative numbers)
 '''
+
 def findRandomMove(validMoves): #validMoves in this case is just the parameter for this def
     return validMoves[random.randint(0, len(validMoves)-1)]
 
@@ -44,10 +44,12 @@ def findBestMove(gs, validMoves): #Helper method to make first recursive call (c
     return bestPlayerMove #Helper method to make
 
 
-def findBestMoveMinMax(gs, validMoves): #Helper def
+def findBestMove1(gs, validMoves): #Helper def
     global nextMove
     nextMove = None
-    findMoveMinMax(gs, validMoves, settings.DEPTH, gs.whiteToMove)
+    # findMoveMinMax(gs, validMoves, settings.DEPTH, gs.whiteToMove)
+    # findMoveNegaMax(gs, validMoves, settings.DEPTH, 1 if gs.whiteToMove else -1)
+    findMoveNegaMaxAlphaBeta(gs, validMoves, settings.DEPTH - 1, -settings.CHECKMATE, settings.CHECKMATE, 1 if gs.whiteToMove else -1)
     return nextMove
 
 def findMoveMinMax(gs, validMoves, depth, whiteToMove): #The true minmax algorithm (with recursion happening)
@@ -82,16 +84,54 @@ def findMoveMinMax(gs, validMoves, depth, whiteToMove): #The true minmax algorit
             gs.undoMove()
         return minScore
 
+def findMoveNegaMax(gs, validMoves, depth, turnMultiplier):
+    global nextMove
+    if depth == 0:
+        return turnMultiplier * scoreBoard(gs)
+    maxScore = -settings.CHECKMATE
+    for move in validMoves:
+        gs.makeMove(move)
+        nextMoves = gs.getValidMove()
+        score = -findMoveNegaMax(gs, nextMoves, depth - 1, -turnMultiplier)
+        if score > maxScore:
+            maxScore = score
+            if depth == settings.DEPTH:
+                nextMove = move
+        gs.undoMove()
+    return maxScore
+
+def findMoveNegaMaxAlphaBeta(gs, validMoves, depth, alpha, beta, turnMultiplier):
+    global nextMove
+    if depth == 0:
+        return turnMultiplier * scoreBoard(gs)
+
+    #Move ordering - implement later
+    maxScore = -settings.CHECKMATE
+    for move in validMoves:
+        gs.makeMove(move)
+        nextMoves = gs.getValidMove()
+        score = -findMoveNegaMaxAlphaBeta(gs, nextMoves, depth - 1, -beta, -alpha, -turnMultiplier)
+        if score > maxScore:
+            maxScore = score
+            if depth == settings.DEPTH:
+                nextMove = move
+        gs.undoMove()
+        if maxScore > alpha: #Pruning happens
+            alpha = maxScore
+        if alpha >= beta:
+            break
+    return maxScore
+
 
 #Positive score --> good for white; negative score --> good for black
 def scoreBoard(gs):
     score = 0
-    if gs.checkmate:
+    if gs.checkMate:
         if gs.whiteToMove:
             return -settings.CHECKMATE #Black wins
         else:
             return settings.CHECKMATE #White wins
-    elif gs.stalemate:
+    elif gs.staleMate:
         return settings.STALEMATE
     for row in gs.board:
         for square in row:
@@ -111,3 +151,50 @@ def scoreMaterial(board):
             elif square[0] == 'b':
                 score -= settings.pieceScore[square[1]]
     return score
+
+'''
+The overall explanation of this file (in order):
+1st: We code findRandomMove in order to let AI make a totally random move. This results in some "stupid" moves because it is randomly selected.
+
+2nd: We code findBestMove. This is when the AI can think about "materials" (each piece has its value (coded in scoreMaterial and scoreBoard))
+which means AI can do moves that captured other pieces for its advantage. However, this code can only loop twice (means think 2 moves in advanced) 
+--> not efficient
+
+3rd: We code findMoveMinMax and findBestMoveMinMax. The findMoveMinMax is used to calculate the best possible score that an AI can do 
+(which results in points rather than the move). Therefore we need findBestMoveMinMax in order to make the move.
+How does the MinMax algorithm work?
+For example we have a tree:
+                        A
+                B              C
+           D(3)   E(-2)    F(1)  G(0)
+The algorithm runs like this.
+Lets say it is black's turn to move. Black's goal is to make the number as negative as possible (whereas white needs the points to be as positive as possible).
+With the depth of 2, the points for move D, E, F, G are 3; -2; 1; 0 respectively. With the goal of making the number as small as possible:
+    On the B branch: We have 3 > -2 --> B = -2
+    On the C branch: We have 1 > 0 --> C = 0
+    On the A branch: We have 0 > -2 --> A = -2
+    Result: with the situation A, black will choose B to move.
+This tree only has the depth of 2. However, when it reaches to the depth of 3 --> the code is very slow.
+In conclusion: MinMax algorithm's goal is to "minimize the maximum's opponent's score.
+But the computer can't stimulate the tree into n depth --> we need something else to reduce the code.
+
+4rd: We code findMoveNegaMax. The same as the 3rd step, we still need to use findBestMoveMinMax (now it is names as findBestMove) in order to convert the move.
+Similar to MinMax, however, MinMax algorithm needs 2 recursions: 1 for min and 1 for max. 
+For NegaMax (negative max), we will see that both sides want to "maximize". 
+Therefore, instead of making 2 recursions, we only just need 1 and then add "-" to switch to the opponent's.
+For example, if white makes a move that marks as +5 --> black will receive -5 (which is bad for black). Therefore, black need to increase the number. For example increase to -2
+Then for white, it will be switched back to 2. Then white tries to increase (increase until reached settings.CHECKMATE).
+Another situation will be like this: if black makes a move that results in 2 --> white will have -2 (which is bad for white). Then white have to increase again.
+In conclusion, instead of using both sides: positive and negative numbers, NegaMax will try both sides increase to  + settings.CHECKMATE
+However, the code is still slow.
+
+5th: We cod findMoveNegaMaxAlphaBeta.
+What is Alpha Beta pruning?
+Alpha is the variable stands for the best score for white, whereas beta is the variable stands for the best score for black.
+For white, it alpha >= beta --> no need to explore further. (The code stops)
+For black, it is the same but everything is change (-beta <= - alpha) since black needs to be as negative as possible. (since we implement the NegaMax).
+
+6th: 
+
+
+'''
